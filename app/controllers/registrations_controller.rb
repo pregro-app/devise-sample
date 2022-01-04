4804e195db87a6cbbb0fd1aa7f0127b141db6851
morioka1206 <40672270+morioka1206@users.noreply.github.com>
2022-01-04 23:55:00 +0900

    userの情報を取り出せないか試している

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,29 @@
 class RegistrationsController < ApplicationController
+
+
   def show
-    @user.new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(id: current_user.id)
     
+    
+    binding.pry
+    
+    @user.save!
+    # セーブができたら
+    redirect_to "http://www.pikawaka.com"
   end
+
+
+  private 
+  def user_params
+    
+    # email = Devise.friendly_token
+    params.require(:user).permit(:id ,:password, :password_confirmation)
+  end
+  
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    :confirmations => 'users/confirmations',
+    # :confirmations => 'users/confirmations',
+    :confirmations => 'registrations',
   }
   devise_scope :user do
-    patch "users/confirm" => "users/confirmations#confirm"
+    # patch "users/confirm" => "users/confirmations#confirm"
+    patch "users/confirm" => "pre_registrations#show"
   end
 
   resource :pre_registration
+  resource :registration
 
 
   # letter_openerのルート
