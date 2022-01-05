7069a118eb201550db3110d8b3348f490a319dcf
morioka1206 <40672270+morioka1206@users.noreply.github.com>
2022-01-05 15:07:36 +0900

    Gmailで送れるようにした

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,14 +34,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # letter_openerを使う
-  config.action_mailer.delivery_method = :letter_opener_web
+  # config.action_mailer.delivery_method = :letter_opener_web
+
+  # Gmailのセッティング
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "morioka.pregro@gmail.com",
+    :password => "kqqssznossbrkuig",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
