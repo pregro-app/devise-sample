6f7fa8a21a9bb60ef9572c74928a9e945d87bf87
morioka1206 <40672270+morioka1206@users.noreply.github.com>
2022-01-07 15:11:46 +0900

    余計な記述を消す

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,11 +13,9 @@ class ApplicationController < ActionController::Base
   #   new_pre_registration
   # end
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :configure_permitted_parameters, if: :devise_controller?
 
-#新規登録時のストロングパラメータに「nicknameカラム」の追加
 def configure_permitted_parameters
-    # 新規登録時のストロングパラメータに「nicknameカラムとageカラム」の追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age])
 end
